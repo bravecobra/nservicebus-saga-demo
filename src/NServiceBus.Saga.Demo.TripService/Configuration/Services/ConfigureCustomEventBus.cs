5b4f0de64db2b044f004ae436aa9a846ea136798
Brave Cobra <bravecobra@gmail.com>
2022-01-06 12:25:24 +0100

    Added monitoring and auditing

--- src/NServiceBus.Saga.Demo.TripService/Configuration/Services/ConfigureCustomEventBus.cs
+++ src/NServiceBus.Saga.Demo.TripService/Configuration/Services/ConfigureCustomEventBus.cs
@@ -28,6 +28,42 @@ namespace NServiceBus.Saga.Demo.TripService.Configuration.Services
                 endpointConfiguration.MakeInstanceUniquelyAddressable(Environment.MachineName);
                 endpointConfiguration.UseSerialization<NewtonsoftSerializer>();
                 endpointConfiguration.SendFailedMessagesTo(errorQueue: "error");
+                endpointConfiguration.AuditProcessedMessagesTo("audit");
+
+                var metrics = endpointConfiguration.EnableMetrics();
+
+                metrics.SendMetricDataToServiceControl(
+                    serviceControlMetricsAddress: "Particular.Monitoring",
+                    interval: TimeSpan.FromSeconds(2)
+                );
+                var servicePlatformConnection = ServicePlatformConnectionConfiguration.Parse(@"{
+    ""Heartbeats"": {
+        ""Enabled"": true,
+        ""HeartbeatsQueue"": ""Particular.Myservicecontrol"",
+        ""Frequency"": ""00:00:10"",
+        ""TimeToLive"": ""00:00:40""
+    },
+    ""MessageAudit"": {
+        ""Enabled"": true,
+        ""AuditQueue"": ""audit""
+    },
+    ""CustomChecks"": {
+        ""Enabled"": true,
+        ""CustomChecksQueue"": ""Particular.Myservicecontrol""
+    },
+    ""ErrorQueue"": ""error"",
+    ""SagaAudit"": {
+        ""Enabled"": true,
+        ""SagaAuditQueue"": ""audit""
+    },
+    ""Metrics"": {
+        ""Enabled"": true,
+        ""MetricsQueue"": ""Particular.Monitoring"",
+        ""Interval"": ""00:00:01""
+    }
+}");
+
+                endpointConfiguration.ConnectToServicePlatform(servicePlatformConnection);
 
                 var routing = transport.Routing();
                 routing.RouteToEndpoint(typeof(SubmitTrip), "TripService");
